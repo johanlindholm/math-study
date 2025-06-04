6261f4927eea6214c2d84556aaf74f25ff158803
Johan Lindholm <johan@lindholm.me>
2025-06-04 20:44:36 +0200

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-// Uncomment the following line if using Prisma Accelerate
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-// Standard Prisma Client
-
-// If using Prisma Accelerate, use this instead:
-export const prisma = globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate())
-
+export const prisma = new PrismaClient().$extends(withAccelerate())
